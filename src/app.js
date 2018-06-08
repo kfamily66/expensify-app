07dca87957909bdac9ae1b2b4369d5a2bca1a581
unknown <kfamily66@yandex.ru>
2018-06-08 18:44:48 +0500

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "normalize.css";
+import "./styles/style.scss";
+
+ReactDOM.render(
+  <p> This is the expensify-app</p>,
+  document.getElementById("app")
+);
