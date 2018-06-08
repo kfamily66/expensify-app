6a371de3082fb673c13ac7e9a15a9255ee1335a4
unknown <kfamily66@yandex.ru>
2018-06-08 19:31:08 +0500

    Setup basic client-side routing with react-router-dom

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { BrowserRouter, Route } from "react-router-dom";
 import "normalize.css";
 import "./styles/style.scss";
 
-ReactDOM.render(
-  <p> This is the expensify-app</p>,
-  document.getElementById("app")
+const ExpensifyDashboardPage = () => <div>This is a dashboard!</div>;
+const AddExpensePage = () => <div>This is a create page!</div>;
+const HelpPage = () => <div>This is a help page!</div>;
+const EditExpensePage = () => <div>This is an edit page!</div>;
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpensifyDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/help" component={HelpPage} />
+      <Route path="/edit" component={EditExpensePage} />
+    </div>
+  </BrowserRouter>
 );
+
+ReactDOM.render(routes, document.getElementById("app"));
