c39d013f879ed6e2011b51541a45cb5310b53d77
unknown <kfamily66@yandex.ru>
2018-06-11 23:17:56 +0500

    Split redux file to separate files and import them to the app.js

---
+++ src/selectors/expenses.js
@@ -0,0 +1,18 @@
+export default (expenses, { text, sortBy, startDate, endDate }) => {
+  return expenses
+    .filter(expense => {
+      const startDateMatch = typeof startDate !== "number" || expense.createdAt >= startDate;
+      const endDateMatch = typeof endDate !== "number" || expense.createdAt <= endDate;
+      const textMatch = text === "" || expense.description.toLowerCase().includes(text.toLowerCase());
+
+      return textMatch && startDateMatch && endDateMatch;
+    })
+    .sort((a, b) => {
+      if (sortBy === "date") {
+        return a.createdAt < b.createdAt ? 1 : -1;
+      }
+      if (sortBy === "amount") {
+        return a.amount < b.amount ? 1 : -1;
+      }
+    });
+};
