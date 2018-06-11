6e606530adcced3d9328c6c4a7794a99deee0d36
unknown <kfamily66@yandex.ru>
2018-06-11 14:07:50 +0500

    New redux action generators using ES6 object desctructuring for arguments

--- src/playground/redux.js
+++ src/playground/redux.js
@@ -1,19 +1,38 @@
 import { createStore } from "redux";
 
+//Actions generators
+
+//Using ES6 object desctricturing for arguments of this function:
+const increment = ({ incrementBy = 1 } = {}) => ({
+  type: "INCREMENT",
+  incrementBy
+});
+
+const decrement = ({ decrementBy = 1 } = {}) => ({
+  type: "DECREMENT",
+  decrementBy
+});
+
+const set = ({ count }) => ({
+  type: "SET",
+  count
+});
+
+const reset = () => ({
+  type: "RESET"
+});
+
+// Creating the redux store and hanlders for possible actions with state
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case "INCREMENT":
-      const incrementBy =
-        typeof action.incrementBy === "number" ? action.incrementBy : 1;
       return {
-        count: state.count + incrementBy
+        count: state.count + action.incrementBy
       };
 
     case "DECREMENT":
-      const decrementBy =
-        typeof action.decrementBy === "number" ? action.decrementBy : 1;
       return {
-        count: state.count - decrementBy
+        count: state.count - action.decrementBy
       };
 
     case "SET":
@@ -31,24 +50,27 @@ const store = createStore((state = { count: 0 }, action) => {
 });
 
 const unsubscribe = store.subscribe(() => console.log(store.getState()));
+// unsubscribe();  // Use unsubscribe to stop tracking changes of state
 
+// Changing state without action generators. There are many possibilites for typos without throwing error.
 store.dispatch({
   type: "INCREMENT",
   incrementBy: 5
 });
-
 store.dispatch({
   type: "RESET"
 });
-
-// unsubscribe();
-
 store.dispatch({
   type: "DECREMENT",
   decrementBy: 3
 });
-
 store.dispatch({
   type: "SET",
   count: 101
 });
+
+// Changing state with action generators. Every typo will cause clear error.
+store.dispatch(increment({ incrementBy: 1000 }));
+store.dispatch(decrement({ decrementBy: 500 }));
+store.dispatch(set({ count: 200 }));
+store.dispatch(reset());
