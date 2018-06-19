7ba915126dc00dd3f7bda9a724002723297ce4cd
unknown <kfamily66@yandex.ru>
2018-06-19 19:45:09 +0500

    Convert EditExpensePage into statefull component and use mapDispatchToProps to abstract dispatch from component

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -3,30 +3,37 @@ import ExpenseForm from "./ExpenseForm";
 import { connect } from "react-redux";
 import { editExpense, removeExpense } from "../actions/expenses";
 
-const EditExpensePage = props => {
-  return (
-    <div>
-      <ExpenseForm
-        expense={props.expense}
-        onSubmit={expense => {
-          props.dispatch(editExpense(props.expense.id, expense));
-          props.history.push("/");
-        }}
-      />
-      <button
-        onClick={e => {
-          props.dispatch(removeExpense({ id: props.expense.id }));
-          props.history.push("/");
-        }}
-      >
-        Remove
-      </button>
-    </div>
-  );
-};
+class EditExpensePage extends React.Component {
+  onSubmit = expense => {
+    this.props.editExpense(this.props.expense.id, expense);
+    this.props.history.push("/");
+  };
+
+  onClick = () => {
+    this.props.removeExpense({ id: this.props.expense.id });
+    this.props.history.push("/");
+  };
+
+  render() {
+    return (
+      <div>
+        <ExpenseForm expense={this.props.expense} onSubmit={this.onSubmit} />
+        <button onClick={this.onClick}>Remove</button>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch, props) => ({
+  editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+  removeExpense: data => dispatch(removeExpense(data))
+});
 
 const mapStateToProps = (state, props) => ({
   expense: state.expenses.find(expense => expense.id === props.match.params.id)
 });
 
-export default connect(mapStateToProps)(EditExpensePage);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(EditExpensePage);
