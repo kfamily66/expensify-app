ae26fe897a612c7f7dc3a000d1e4dc88fdce8b97
unknown <kfamily66@yandex.ru>
2018-06-21 13:17:28 +0500

    Setup package.json for heroku

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const app = express();
 const publicPath = path.join(__dirname, "..", "public");
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
@@ -11,4 +11,4 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
 
-app.listen(PORT, () => console.log(`Server is up on port ${PORT}`));
+app.listen(port, () => console.log(`Server is up on port ${port}`));
