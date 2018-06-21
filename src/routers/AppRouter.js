e79d3881e6108d75eeb28be587c7c0797f220cd5
unknown <kfamily66@yandex.ru>
2018-06-21 12:16:29 +0500

    Remove HeplPage from app

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
 import AddExpensePage from "../components/AddExpensePage";
 import Header from "../components/Header";
-import HelpPage from "../components/HelpPage";
 import NotFoundPage from "../components/NotFoundPage";
 import ExpensifyDashboardPage from "../components/ExpensifyDashboardPage";
 import EditExpensePage from "../components/EditExpensePage";
@@ -14,7 +13,6 @@ const AppRouter = () => (
       <Switch>
         <Route path="/" component={ExpensifyDashboardPage} exact={true} />
         <Route path="/create" component={AddExpensePage} />
-        <Route path="/help" component={HelpPage} />
         <Route path="/edit/:id" component={EditExpensePage} />
         <Route component={NotFoundPage} />
       </Switch>
