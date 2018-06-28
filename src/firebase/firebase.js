399cc57448fbf14905ef23a75ed507b3da41a407
unknown <kfamily66@yandex.ru>
2018-06-28 18:17:15 +0500

    Setup test database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,31 +1,39 @@
 import * as firebase from "firebase";
-import config from "./config";
 
-const expenses = [
-  {
-    description: "Coffee",
-    note: "",
-    amount: 300,
-    createdAt: 1516
-  },
-  {
-    description: "Rent",
-    note: "",
-    amount: 1095,
-    createdAt: 6487987
-  },
-  {
-    description: "Gas bill",
-    note: "",
-    amount: 500,
-    createdAt: 48798798
-  }
-];
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIRABASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+};
 
 firebase.initializeApp(config);
 const database = firebase.database();
 
 export { firebase, database as default };
+
+// const expenses = [
+//   {
+//     description: "Coffee",
+//     note: "",
+//     amount: 300,
+//     createdAt: 1516
+//   },
+//   {
+//     description: "Rent",
+//     note: "",
+//     amount: 1095,
+//     createdAt: 6487987
+//   },
+//   {
+//     description: "Gas bill",
+//     note: "",
+//     amount: 500,
+//     createdAt: 48798798
+//   }
+// ];
 
 ///----- CREATE DATA -----///
 
