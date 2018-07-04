2530a8016b11b5a7331549fed88695581b14dbf3
unknown <kfamily66@yandex.ru>
2018-07-04 12:37:36 +0500

    Add public routes

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -6,12 +6,12 @@ import { startEditExpense, startRemoveExpense } from "../actions/expenses";
 export class EditExpensePage extends React.Component {
   onSubmit = expense => {
     this.props.startEditExpense(this.props.expense.id, expense);
-    this.props.history.push("/dashboard");
+    this.props.history.push("/");
   };
 
   onClick = () => {
     this.props.startRemoveExpense({ id: this.props.expense.id });
-    this.props.history.push("/dashboard");
+    this.props.history.push("/");
   };
 
   render() {
